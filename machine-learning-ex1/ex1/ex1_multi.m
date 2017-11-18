771f918023be119e874363d91ee5a57c3faab7d5
Chris Patuzzo <chris@patuzzo.co.uk>
2017-11-18 19:00:43 +0000

    Predict the price of a 1650 square feed, 3 bedroom house

--- machine-learning-ex1/ex1/ex1_multi.m
+++ machine-learning-ex1/ex1/ex1_multi.m
@@ -114,8 +114,7 @@ alpha = 0.3;
 % ====================== YOUR CODE HERE ======================
 % Recall that the first column of X is all-ones. Thus, it does
 % not need to be normalized.
-price = 0; % You should change this
-
+price = [1, 1650, 3] * theta;
 
 % ============================================================
 
