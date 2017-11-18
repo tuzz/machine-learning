da248b74323d4dec1325239423aab9a83019dea8
Chris Patuzzo <chris@patuzzo.co.uk>
2017-11-18 19:22:37 +0000

    Use the normal equation to calculate the linear regression line

--- machine-learning-ex1/ex1/normalEqn.m
+++ machine-learning-ex1/ex1/normalEqn.m
@@ -1,6 +1,6 @@
 function [theta] = normalEqn(X, y)
-%NORMALEQN Computes the closed-form solution to linear regression 
-%   NORMALEQN(X,y) computes the closed-form solution to linear 
+%NORMALEQN Computes the closed-form solution to linear regression
+%   NORMALEQN(X,y) computes the closed-form solution to linear
 %   regression using the normal equations.
 
 theta = zeros(size(X, 2), 1);
@@ -10,13 +10,8 @@ theta = zeros(size(X, 2), 1);
 %               to linear regression and put the result in theta.
 %
 
-% ---------------------- Sample Solution ----------------------
-
-
-
-
-% -------------------------------------------------------------
-
+Xt = X';
+theta = pinv(Xt * X) * Xt * y;
 
 % ============================================================
 
