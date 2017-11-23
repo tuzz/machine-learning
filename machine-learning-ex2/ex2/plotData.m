804cdfd8df6b05a6a523a6f6e1ec2b38bd5967f7
Chris Patuzzo <chris@patuzzo.co.uk>
2017-11-23 13:27:31 +0000

    Plot the exam scores data

--- machine-learning-ex2/ex2/plotData.m
+++ machine-learning-ex2/ex2/plotData.m
@@ -1,5 +1,5 @@
 function plotData(X, y)
-%PLOTDATA Plots the data points X and y into a new figure 
+%PLOTDATA Plots the data points X and y into a new figure
 %   PLOTDATA(x,y) plots the data points with + for the positive examples
 %   and o for the negative examples. X is assumed to be a Mx2 matrix.
 
@@ -12,17 +12,19 @@ figure; hold on;
 %               examples and 'ko' for the negative examples.
 %
 
+% Find the indices of positive and negative training examples.
+positive_indices = find(y == 1);
+negative_indices = find(y == 0);
 
+% Fetch the scores for the positive and negative indices.
+positive_scores = X(positive_indices, [1 2]);
+negative_scores = X(negative_indices, [1 2]);
 
-
-
-
-
-
+% Plot '+' for positives and 'o' for negatives.
+plot(positive_scores(:, 1), positive_scores(:, 2), 'k+', 'MarkerSize', 7, 'LineWidth', 2);
+plot(negative_scores(:, 1), negative_scores(:, 2), 'ko', 'MarkerSize', 7, 'MarkerFaceColor', 'y');
 
 % =========================================================================
-
-
 
 hold off;
 
