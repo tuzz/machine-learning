2b14d229c0e61c631000f6b3d479c900f9847eff
Chris Patuzzo <chris@patuzzo.co.uk>
2017-11-25 15:09:13 +0000

    Implement the cost function for logistic regression

--- machine-learning-ex2/ex2/costFunction.m
+++ machine-learning-ex2/ex2/costFunction.m
@@ -7,26 +7,24 @@ function [J, grad] = costFunction(theta, X, y)
 % Initialize some useful values
 m = length(y); % number of training examples
 
-% You need to return the following variables correctly 
-J = 0;
-grad = zeros(size(theta));
+% Compute the cost function for logistic regression. We need to use the sigmoid
+% function for hTheta so that the cost function is convex and will work with
+% gradient descent (or more sophisticated minimisation algorithms.
+hTheta = sigmoid(X * theta);
 
-% ====================== YOUR CODE HERE ======================
-% Instructions: Compute the cost of a particular choice of theta.
-%               You should set J to the cost.
-%               Compute the partial derivatives and set grad to the partial
-%               derivatives of the cost w.r.t. each parameter in theta
-%
-% Note: grad should have the same dimensions as theta
-%
+% Compute the y=1 and y=0 components of the cost function then combine them in
+% a way that eliminates 'termForYIsOne' when y=0 and eliminates 'termForYIsZero'
+% when y=1.
+termForYIsOne = y .* log(hTheta);
+termForYIsZero = (1 - y) .* log(1 - hTheta);
+costs = -(termForYIsOne + termForYIsZero);
 
+% The cost is the average cost for all training examples.
+J = sum(costs) / m;
 
-
-
-
-
-
-
-% =============================================================
+% Compute the gradient for each parameter of the model. Gradient descent uses
+% this to determine which direction to step (i.e. whether to add/subtract from
+% the parameter) in and how big a step it should take. We can reuse hTheta.
+grad = X' * (hTheta - y) / m;
 
 end
