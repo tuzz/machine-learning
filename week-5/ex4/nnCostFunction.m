9dce5df4890759e72ea45a81e16c97071234c02c
Chris Patuzzo <chris@patuzzo.co.uk>
2017-11-29 20:39:07 +0000

    Implement the cost function using feedforward propagation

--- week-5/ex4/nnCostFunction.m
+++ week-5/ex4/nnCostFunction.m
@@ -8,8 +8,8 @@ function [J grad] = nnCostFunction(nn_params, ...
 %   [J grad] = NNCOSTFUNCTON(nn_params, hidden_layer_size, num_labels, ...
 %   X, y, lambda) computes the cost and gradient of the neural network. The
 %   parameters for the neural network are "unrolled" into the vector
-%   nn_params and need to be converted back into the weight matrices. 
-% 
+%   nn_params and need to be converted back into the weight matrices.
+%
 %   The returned parameter grad should be a "unrolled" vector of the
 %   partial derivatives of the neural network.
 %
@@ -24,8 +24,8 @@ Theta2 = reshape(nn_params((1 + (hidden_layer_size * (input_layer_size + 1))):en
 
 % Setup some useful variables
 m = size(X, 1);
-         
-% You need to return the following variables correctly 
+
+% You need to return the following variables correctly
 J = 0;
 Theta1_grad = zeros(size(Theta1));
 Theta2_grad = zeros(size(Theta2));
@@ -46,12 +46,12 @@ Theta2_grad = zeros(size(Theta2));
 %         that your implementation is correct by running checkNNGradients
 %
 %         Note: The vector y passed into the function is a vector of labels
-%               containing values from 1..K. You need to map this vector into a 
+%               containing values from 1..K. You need to map this vector into a
 %               binary vector of 1's and 0's to be used with the neural network
 %               cost function.
 %
 %         Hint: We recommend implementing backpropagation using a for-loop
-%               over the training examples if you are implementing it for the 
+%               over the training examples if you are implementing it for the
 %               first time.
 %
 % Part 3: Implement regularization with the cost function and gradients.
@@ -62,27 +62,32 @@ Theta2_grad = zeros(size(Theta2));
 %               and Theta2_grad from Part 2.
 %
 
+% Include the bias unit in the input layer.
+X = [ones(m, 1) X];
+
+% Compute the activations of the hidden units.
+hiddenActivations = sigmoid(X * Theta1');
+
+% Include the bias unit in the hidden layer.
+m2 = size(hiddenActivations, 1);
+hiddenActivations = [ones(m2, 1) hiddenActivations];
+
+% Compute the activations of the output units.
+outputActivations = sigmoid(hiddenActivations * Theta2');
+
+% Build a matrix of expected outputs for each training example.
+expectedOutputs = [1:num_labels] == y;
+
+% Apply the logistic regression cost function.
+termForYIsOne = expectedOutputs .* log(outputActivations);
+termForYIsZero = (1 - expectedOutputs) .* log(1 - outputActivations);
+costs = -(termForYIsOne + termForYIsZero);
+
+% The cost is the sum of all output neuron costs for all training examples.
+J = sum(sum(costs)) / m;
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-% -------------------------------------------------------------
-
-% =========================================================================
 
 % Unroll gradients
 grad = [Theta1_grad(:) ; Theta2_grad(:)];
