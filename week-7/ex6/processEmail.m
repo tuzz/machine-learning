61f5acc088c927076c92a742333004246694eb72
Chris Patuzzo <chris@patuzzo.co.uk>
2017-12-31 17:26:04 +0000

    Map words to indices when preprocessing emails

--- week-7/ex6/processEmail.m
+++ week-7/ex6/processEmail.m
@@ -97,13 +97,14 @@ while ~isempty(email_contents)
     %       str2). It will return 1 only if the two strings are equivalent.
     %
 
+    for i = 1:size(vocabList)
+      v = vocabList(i);
+      word = v(1);
 
-
-
-
-
-
-
+      if (strcmp(str, word))
+        word_indices = [word_indices; i];
+      end
+    end
 
 
     % =============================================================
